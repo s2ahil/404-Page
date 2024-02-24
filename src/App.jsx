3d915527fc9eb9c067a6359a94684ccc38e3c4de
s2ahil <sahilpradhan411@gmail.com>
2024-02-24 14:54:36 +0530

    updated home page ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import img from "./images/background.png"
 import Arrow from "../public/svg/hand-drawn-arrow.svg"
 import { useState } from "react";
-
-
+import Message from "../public/svg/message.svg";
+import confused from "./images/confused.png"
 function App() {
   const [showArray, setShowArray] = useState(false);
   const answers = [{
@@ -21,25 +21,22 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col h-[100vh] justify-center items-center '>
+      <div className='flex flex-col h-full justify-center items-center m-4'>
 
-        {/* <div className='flex gap-4 flex-col w-full h-[100vh] items-center justify-center '> */}
+ 
         <div className="grid  
 
-          p-4 ">
-          <div className="flex gap-4 flex-col items-center justify-center">
-            <div className='text-5xl' >
-              4
-              <span>0</span>
-              4</div>
-
-            <div className='text-5xl' >Page not found</div>
-            <div className="inline-flex items-center justify-center w-full">
-              <hr className="w-64 h-[3px] my-2 bg-gray-200 border-0 dark:bg-gray-700" />
-
+          p-4 gap-5">
+          <div className="flex gap-8 flex-col items-center justify-center">
+            <div className='text-5xl ' >
+              <span className="flex gap-1 justify-center items-center text-5xl">4 <img className="" src={Message}></img>4</span>
             </div>
 
-            <button className="bg-[#FFC900] p-4 mt-4 border-[1px] border-black  rounded-xl font-bold" id="GoBackButton">Return Back  :)</button>
+            <img src={confused} className="h-[10rem] "></img>
+            <div className='text-5xl underline_higlight' >Page not found</div>
+
+      
+            <button className="bg-[#FFC900] p-4  border-[1px] border-black  rounded-xl font-bold" id="GoBackButton">Return Back  :)</button>
           </div>
 
 
@@ -49,17 +46,19 @@ function App() {
 
 
         {/* little desc of error */}
-        <div className="flex flex-col  items-center  w-full p-4 h-[10rem]">
-          <div className="cursor-pointer hover:text-blue-500 font-semibold" onClick={() => setShowArray(!showArray)}>Why this page ?</div>
+        <div className="flex flex-col  items-center  w-full p-4 h-[12rem] ">
+          <div className="cursor-pointer hover:text-blue-700 font-semibold underline_highlight text-xl italic highlight " onClick={() => setShowArray(!showArray)}>Why this page ?</div>
 
           {showArray && (
-            <div className="mt-4">
-              <ul className="list-disc "  >
+
+            <div className="mt-4  ">
+              <ul className="list-disc font-semibold italic"  >
                 {answers.map((answer, index) => (
                   <li className="m-2" key={index}>{answer.error}</li>
                 ))}
               </ul>
             </div>
+
           )}
         </div>
       </div>
